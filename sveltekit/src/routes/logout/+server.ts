d62caf8ef2cdb0cdba43330dbb2afca8a78b8a14
Bruenner <bruenner@tugraz.at>
2026-04-18 10:50:28 +0200

    logout 303

--- sveltekit/src/routes/logout/+server.ts
+++ sveltekit/src/routes/logout/+server.ts
@@ -3,16 +3,17 @@ import type { RequestHandler } from './$types';
 import { resolve } from '$app/paths';
 
 export const GET: RequestHandler = async ({ cookies }) => {
-	cookies.delete('jwt', { path: resolve('/') });
+	cookies.delete('jwt', {
+		path: resolve('/')
+	});
 
 	return new Response(null, {
-		status: 302,
+		status: 303,
 		headers: {
-			location: resolve('/'),
+			location: resolve('/login'),
 			'cache-control': 'no-store, no-cache, must-revalidate, proxy-revalidate',
 			pragma: 'no-cache',
-			expires: '0',
-			'clear-site-data': '"cache", "storage"'
+			expires: '0'
 		}
 	});
 };
