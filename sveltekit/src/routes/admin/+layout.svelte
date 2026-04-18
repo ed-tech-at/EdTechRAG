bb3c317803d66413e328d52f5da3a720cc5d3921
Bruenner <bruenner@tugraz.at>
2026-04-18 10:46:15 +0200

    logout data-sveltekit-reload

--- sveltekit/src/routes/admin/+layout.svelte
+++ sveltekit/src/routes/admin/+layout.svelte
@@ -6,6 +6,6 @@
 </script>
 <nav>
   <b>EdTechRAG</b> <a href={resolve('/admin/')}>Admin</a> 
-  <a href={resolve('/logout/')}>Logout</a> 
+  <a href={resolve('/logout')} data-sveltekit-reload>Logout</a> 
 </nav>
 <div class="page-content">{@render children()}</div>
